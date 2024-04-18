b38da6745a58f65e6373ad5148d5d14cea17e1af
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-04-18 19:44:54 +0900

    :iphone: add responsive

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div class="px-4 py-8 mx-auto bg-[#603bff]">
       <div class="max-w-screen-lg mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold text-[#eaff3d]">
+        <h1 class="md:text-4xl sm:text-3xl font-bold text-[#eaff3d]">
           Splatoon3 ランダム二つ名
         </h1>
 
-        <div class="grid grid-cols-2 gap-2 mt-4">
+        <div class="grid md:grid-cols-2 sm:grid-cols-1 gap-2 mt-4">
           {titleImages.map((titleImage) => {
             return <TitleCard titleImage={titleImage} />;
           })}
